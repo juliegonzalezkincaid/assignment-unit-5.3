49b5f17fa9f78e3318f6727c302fe3f5c9ccec7e
julie gonzalez-kincaid <juliegonzalezkincaid@gmail.com>
2023-02-06 14:04:28 -0500

    first part creating function and a variable with an open array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,20 @@
+
 console.log('***** Music Collection *****')
+
+//Create a variable `collection` that starts as an empty array.
+let collection = [];
+
+ function addToCollection() { //Add a function named addToCollection
+    let musicInfo = {  //Create a new object having the above properties
+
+        title: '',
+        artist: '',
+        yearPublished: 0,
+
+
+    };
+    collection.push(musicInfo); //Add the new object to the end of the collection array
+    return musicInfo; //Return the newly created object
+
+ };
+ 
