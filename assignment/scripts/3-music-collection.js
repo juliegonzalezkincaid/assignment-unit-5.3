fe73a19a4a8302e01fc16621c52eb6b47f884aba
julie gonzalez-kincaid <juliegonzalezkincaid@gmail.com>
2023-02-06 16:52:55 -0500

    Console logged add to coollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,26 +4,56 @@ console.log('***** Music Collection *****')
 //Create a variable `collection` that starts as an empty array.
 let collection = [];
 
- function addToCollection() { //Add a function named addToCollection
+ function addToCollection(title, artist, yearPublished) { //Add a function named addToCollection
     let musicInfo = {  //Create a new object having the above properties
-
-        title: '',
-        artist: '',
-        yearPublished: 0,
-
-
-    };
+        title,
+        artist,
+        yearPublished,
+        };
     collection.push(musicInfo); //Add the new object to the end of the collection array
-    return musicInfo; //Return the newly created object
+    console.log(collection)
+    return musicInfo;
+    //Return the newly created object
 
- };
+ }
 
  //Test the addToCollection function:
-
 //Add 6 albums to your collection
- console.log('The Jimi Hendrix Experience', 'Jimi Hendrix', 1967);
- console.log('MTV Unpluggged in New York', 'Nirvana', 1994);
- console.log('The Dark Side of the Moon', 'Pink Floyd', 1973);
-    console.log('Standing on a Beach', 'The Cure' , 1986);
-        console.log('Ok Computer', 'Radiohead', 1995);
-            console.log('Songs in the Key of Life', 'Stevie Wonder', 1976);
+//After all are added, console.log the collection array.
+ console.log(addToCollection('The Jimi Hendrix Experience', 'Jimi Hendrix', 1967));
+ console.log(addToCollection('MTV Unpluggged in New York', 'Nirvana', 1994));
+ console.log(addToCollection('The Dark Side of the Moon', 'Pink Floyd', 1973));
+ console.log(addToCollection('Standing on a Beach', 'The Cure' , 1986));
+ console.log(addToCollection('Ok Computer', 'Radiohead', 1995));
+ console.log(addToCollection('Songs in the Key of Life', 'Stevie Wonder', 1976));
+
+            console.table(collection);
+            console.log(collection)
+
+
+
+
+
+
+function showCollection(array){
+    console.log('showcollection', array);
+
+
+}
+
+
+
+
+function findByArtist(){
+
+
+}
+
+
+
+
+
+  //contentDiv.innerHTML += 
+    //    ${musicInfo.title}
+      //  ${musicInfo.artist}
+        //${musicInfo.yearPublished}           
